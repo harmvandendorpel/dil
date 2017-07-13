bb0a4ccf14d12999f233e3ab4566fe164ff6d45f
Harm van den Dorpel <harm@server.delinear.info>
2017-07-13 14:33:45 +0200

    server

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+app.use(bodyParser.json({ limit: '100mb' }));
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
